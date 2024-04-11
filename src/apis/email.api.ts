58b45c89bf06e32ca8b1782f48485304bc1948f2
Stanislau Ihnatsenka <stanislau.ihnatsenka@hiqo-solutions.com>
2024-04-11 16:50:43 +0200

    add caching for emails api

--- src/apis/email.api.ts
+++ src/apis/email.api.ts
@@ -1,12 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Email } from '../types/email.type';
+import { TagTypes } from './tag-types.enum';
 
 export const emailApi = createApi({
   reducerPath: 'email-api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  tagTypes: [TagTypes.Email],
   endpoints: (builder) => ({
     getEmails: builder.query({
       query: () => '/emails',
+      providesTags: [TagTypes.Email],
     }),
     sendEmail: builder.mutation({
       query: (email: Email) => ({
@@ -14,6 +17,7 @@ export const emailApi = createApi({
         method: 'POST',
         body: email,
       }),
+      invalidatesTags: [TagTypes.Email]
     }),
   }),
 });
