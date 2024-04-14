c5767b318b286ffaa0b0eed4a02e7f854d10b516
Stanislau Ihnatsenka <stanislau.ihnatsenka@hiqo-solutions.com>
2024-04-14 10:16:49 +0200

    remove unused files

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const userApi = createApi({
+  reducerPath: 'user-api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
   endpoints: (builder) => ({
     getUsers: builder.query({
