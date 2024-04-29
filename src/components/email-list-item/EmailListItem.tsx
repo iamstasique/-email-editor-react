8e7c661d40b6ae66ff187ba1b8d7ccb4e5393dca
Stanislau Ihnatsenka <stanislau.ihnatsenka@hiqo-solutions.com>
2024-04-29 11:13:43 +0200

    add material ui library

--- src/components/email-list-item/EmailListItem.tsx
+++ src/components/email-list-item/EmailListItem.tsx
@@ -1,20 +1,22 @@
+import { Card, CardContent, Typography } from '@mui/material';
 import parse from 'html-react-parser';
+import { useGetUserQuery } from '../../apis/user.api';
 import { localStorageService } from '../../services/local-storage.service';
 import { ANOTHER_USER_STYLES, CURRENT_USER_STYLES } from '../../styles/styles.constant';
 import { Email } from '../../types/email.type';
 import style from './EmailListItem.module.scss';
-import { useGetUserQuery } from '../../apis/user.api';
 
 export function EmailListItem({ email }: { email: Email }) {
   const { data: sender } = useGetUserQuery(email.senderId);
   const isCurrentUser = sender?.id === localStorageService.getCurrentUserId();
 
   return (
-    <div className={style['email-list-item']} style={isCurrentUser ? CURRENT_USER_STYLES : ANOTHER_USER_STYLES}>
-      <div className={style['email-list-item-sender']}>{sender?.name}</div>
-      <span className={style['email-list-item-content']}>{parse(email.text)}</span>
-      <br />
-      <div className={style['email-list-item-date']}>{new Date(email.date).toLocaleTimeString()}</div>
-    </div>
+    <Card sx={{overflow: 'initial', backgroundColor: "inherit"}}>
+      <CardContent className={style['email-list-item']} style={isCurrentUser ? CURRENT_USER_STYLES : ANOTHER_USER_STYLES}>
+        <Typography className={style['email-list-item-sender']}>{sender?.name}</Typography>
+        <Typography className={style['email-list-item-content']}>{parse(email.text)}</Typography>
+        <Typography className={style['email-list-item-date']}>{new Date(email.date).toLocaleTimeString()}</Typography>
+      </CardContent>
+    </Card>
   );
 }
